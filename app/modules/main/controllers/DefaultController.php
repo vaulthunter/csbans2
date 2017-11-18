e3ffe1113e3e6877bb97cbf7b6783b166bbcc6c5
Santiago <alejandro@de-salvatierra.ru>
2017-11-18 13:16:10 +0300

    Лишнее

--- app/modules/main/controllers/DefaultController.php
+++ app/modules/main/controllers/DefaultController.php
@@ -2,41 +2,13 @@
 
 namespace modules\main\controllers;
 
-use yii\filters\AccessControl;
 use yii\web\Controller;
-use yii\filters\VerbFilter;
 
 /**
  * Default controller for the `main` module
  */
 class DefaultController extends Controller
 {
-    /**
-     * @inheritdoc
-     */
-    public function behaviors()
-    {
-        return [
-            'access' => [
-                'class' => AccessControl::className(),
-                'only' => ['logout'],
-                'rules' => [
-                    [
-                        'actions' => ['logout'],
-                        'allow' => true,
-                        'roles' => ['@'],
-                    ],
-                ],
-            ],
-            'verbs' => [
-                'class' => VerbFilter::className(),
-                'actions' => [
-                    'logout' => ['post'],
-                ],
-            ],
-        ];
-    }
-
     /**
      * @inheritdoc
      */
